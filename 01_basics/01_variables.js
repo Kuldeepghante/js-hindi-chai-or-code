ae424d830abd01157a523583ef56fe7a4017d2fe
Kuldeepghante <124690392+Kuldeepghante@users.noreply.github.com>
2025-04-19 06:41:58 +0000

    add initial variable declarations and console log

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId  = 1234567890;
+//const never changes //
+ const accountEmail="kuldeep@gmail.com"
+ var accountPassword="1234567890"
+  accountcity="delhi"
+  accountstate=;
+
+ //prefer not use var because of issue in block scope and functional scope //
+
+// accountId = 2 is not allowed //
+
+
+console.log(accountId, accountEmail, accountPassword, accountcity, accountstate);
+
