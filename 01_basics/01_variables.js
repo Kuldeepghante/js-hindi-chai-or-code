9185d562b50def1a2ada92f870ca2fc97de20c04
Kuldeepghante <124690392+Kuldeepghante@users.noreply.github.com>
2025-04-19 06:55:30 +0000

    fix: correct variable name and improve comments in variable declaration

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,13 +2,14 @@ const accountId  = 1234567890;
 //const never changes //
  const accountEmail="kuldeep@gmail.com"
  var accountPassword="1234567890"
+  //prefer not use var because of issue in block scope and functional scope //
   accountcity="delhi"
-  accountstate=;
-
- //prefer not use var because of issue in block scope and functional scope //
+  accountState="";
 
 // accountId = 2 is not allowed //
 
 
-console.log(accountId, accountEmail, accountPassword, accountcity, accountstate);
+console.log(accountId, accountEmail, accountPassword, accountcity, accountState);
+
+console.table([accountId, accountEmail, accountPassword, accountcity, accountState]);
 
